fd7cd5ddd6ddbe22f14a4c3d3e474b4fcaf1faeb
Abhishek Ruhela <94753084+mysterioxx@users.noreply.github.com>
2026-03-29 18:05:23 +0530

    Initial Commit

---
+++ NetPulse/NetPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetPulseApp.swift
+//  NetPulse
+//
+//  Created by Abhishek Ruhela on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct NetPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
